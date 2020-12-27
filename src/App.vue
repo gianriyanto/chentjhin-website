2d761fc094dc4c8a3a54039960019315088ceed9
gianriyanto <li.griyanto@gmail.com>
2020-12-27 11:31:09 +1100

    Refactored fonts directory to assets folder

--- src/App.vue
+++ src/App.vue
@@ -45,72 +45,72 @@ html, body {
 @font-face {
   font-family: "Gilroy Bold";
   src: local("Gilroy Bold"),
-  url(./fonts/Gilroy/Gilroy-Bold.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Bold.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy Medium";
   src: local("Gilroy Medium"),
-  url(./fonts/Gilroy/Gilroy-Medium.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Medium.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy Regular";
   src: local("Gilroy Regular"),
-  url(./fonts/Gilroy/Gilroy-Regular.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Regular.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy SemiBold";
   src: local("Gilroy SemiBold"),
-  url(./fonts/Gilroy/Gilroy-SemiBold.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-SemiBold.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy Heavy";
   src: local("Gilroy Heavy"),
-  url(./fonts/Gilroy/Gilroy-Heavy.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Heavy.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy Light";
   src: local("Gilroy Light"),
-  url(./fonts/Gilroy/Gilroy-Light.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Light.ttf) format("truetype");
 }
 @font-face {
   font-family: "Gilroy Thin";
   src: local("Gilroy Thin"),
-  url(./fonts/Gilroy/Gilroy-Thin.ttf) format("truetype");
+  url(./assets/fonts/Gilroy/Gilroy-Thin.ttf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica ExtraBold";
   src: local("Bw Modelica ExtraBold"),
-  url(./fonts/BwModelica/BwModelica-ExtraBold.ttf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-ExtraBold.ttf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Bold";
   src: local("Bw Modelica Bold"),
-  url(./fonts/BwModelica/BwModelica-Bold.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Bold.otf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Medium";
   src: local("Bw Modelica Medium"),
-  url(./fonts/BwModelica/BwModelica-Medium.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Medium.otf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Regular";
   src: local("Bw Modelica Regular"),
-  url(./fonts/BwModelica/BwModelica-Regular.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Regular.otf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Light";
   src: local("Bw Modelica Light"),
-  url(./fonts/BwModelica/BwModelica-Light.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Light.otf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Thin";
   src: local("Bw Modelica Thin"),
-  url(./fonts/BwModelica/BwModelica-Thin.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Thin.otf) format("truetype");
 }
 @font-face {
   font-family: "Bw Modelica Hairline";
   src: local("Bw Modelica Hairline"),
-  url(./fonts/BwModelica/BwModelica-Hairline.otf) format("truetype");
+  url(./assets/fonts/BwModelica/BwModelica-Hairline.otf) format("truetype");
 }
 
 
