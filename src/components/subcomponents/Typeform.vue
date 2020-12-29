25ceb01a38c9b3e44879cef7414c8e67a37c7164
gianriyanto <li.griyanto@gmail.com>
2020-12-29 23:50:15 +1100

    Clean up code in InlineInput.vue and Typeform.vue

--- src/components/subcomponents/Typeform.vue
+++ src/components/subcomponents/Typeform.vue
@@ -238,17 +238,13 @@ export default {
   }
 }
 
-/* slide fade transition */
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
 .slide-fade-enter-active {
   transition: all 1s ease-in-out;
 }
 .slide-fade-leave-active {
   transition: all 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
-  /* .slide-fade-leave-active below version 2.1.8 */ {
+.slide-fade-enter, .slide-fade-leave-to{
   transform: translateY(400px) scale(0.7);
   opacity: 0;
 }
